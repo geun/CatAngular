c705aaa9ef4c12f6641892ada15f035f30315423
geun-mac <geun@touchingsignal.com>
2013-04-23 22:05:41 +0900

    Revert "Revert "Revert "add haml engine for assets"""
    This reverts commit e18f579e54271ddcf0da288cbddd5a73a83631ef.

--- config/application.rb
+++ config/application.rb
@@ -76,9 +76,6 @@ module CatAngular
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.register_engine '.haml', Tilt::HamlTemplate
-
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
