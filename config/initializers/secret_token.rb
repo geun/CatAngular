ec3ebd9c2415230a44a57e388b110e3cf580cd45
geun-mac <geun@touchingsignal.com>
2013-04-23 19:30:54 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatAngular::Application.config.secret_token = 'eb043d833a584c150a75d7935d8e96dec397bf48c9c513f702ac92dcce42049165ac69e7e235a8d1252f297d23753aa33b642fbe7210e364a8c8afb8b325db6c'
